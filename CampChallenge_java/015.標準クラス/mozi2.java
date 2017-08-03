7322b973bd176be08cb2f9851007a1eec8fb2831
EXP-0009\guest1Day <ayakata128@gmail.com>
2017-08-03 11:26:07 +0900

    標準クラス　文字数の取得を提出します

---
+++ CampChallenge_java/015.標準クラス/mozi2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Word;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class mozi2 {
+    public static void main(String[] args){
+        String mail = "ayakata128@gmail.com";
+        //「@」以降の文字数を取得
+        System.out.print(mail.substring(10).length());
+    }
+}
